1512f66f8d48faee6fe13052a2e2ff488ae6c883
ranjit wagh <ranjitw2522@gmail.com>
2019-01-18 00:49:34 +0530

    added 1-6 testcases from Mars-Group5

--- Project/OpenMRS/openMRS_Page_Locators/Login_Page_Locator.java
+++ Project/OpenMRS/openMRS_Page_Locators/Login_Page_Locator.java
@@ -1,5 +1,7 @@
 package openMRS_Page_Locators;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,29 +9,37 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Login_Page_Locator {
 	WebDriver driver = null;
-	public Login_Page_Locator(WebDriver driver){
+
+	public Login_Page_Locator(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(id = "username")
 	private WebElement userName;
-	
-	public WebElement getUserID(){
+
+	public WebElement getUserID() {
 		return userName;
 	}
-	
+
 	@FindBy(id = "password")
 	private WebElement password;
-	
-	public WebElement getPassword(){
+
+	public WebElement getPassword() {
 		return password;
 	}
-	
+
+	@FindBy(xpath = "//ul[@id='sessionLocation']/li")
+	private List<WebElement> locationOptions;
+
+	public List<WebElement> getLocationOptions() {
+		return locationOptions;
+	}
+
 	@FindBy(id = "loginButton")
 	private WebElement loginBtn;
-	
-	public WebElement getLoginBtn(){
+
+	public WebElement getLoginBtn() {
 		return loginBtn;
 	}
 }
